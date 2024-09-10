f998131f1c261beab4e872ffa477fa19d21ddc6e
mrcallaghan <mcallaghan@naperville203.org>
2024-09-10 08:25:44 -0500

    wrote notes on methods and intance variables

--- MileageTracker.java
+++ MileageTracker.java
@@ -1,15 +1,28 @@
 /**
  * This class models a mileage tracker for a car.
  *
- * @author 
- * @version 
+ * @author mrcallaghan
+ * @version 10sep2024
  */
 public class MileageTracker
 {
     /*
      * 2. Define the instance variables (store the object's attributes):
+     *      specify the visibility (e.g., private)
+     *          public: accessible by any code in any class
+     *          private: only accessible by mehtods in this class
+     *      specify the type (e.g., int, double, String)
+     *      specify the name (e.g., distanceDriven)
+     *      
+     *      Instance variables differ from local variables in the folowing ways:
+     *          scoped to the entire class (accessible in all methods of the class;
+     *                                      lifetime is the same of the object)
+     *          automatically initialized to a default value (0, false, null)
+     *          best pratice is not to immediately initialize instance vairables
      */
-
+    private int distanceDriven;  // in units of miles
+    private int fuelConsumed;    // in units of gallons
+    private String vin;          // vehicle identification number
 
 
 
@@ -29,9 +42,32 @@ public class MileageTracker
     
     /*
      * 1. Define methods by specifying:
+     *      the visibility (e.g., public, private)
+     *      the return type (e.g., void, int)
+     *      the method name (e.g., incrementDistanceDriven, getDistanceDriven, etc.)
+     *      the parameters and thier types (e.g., miles of type int)
+     *      
      */
     
+    /**
+     * Increments the number of miles that the car has driven.
+     * 
+     * @param miles the additional distance, in miles, this car has driven.
+     */
+    public void incrementDistanceDriven(int miles)
+    {
+        return;
+    }
     
+    /**
+     * Returns the total number of miles driven.
+     * 
+     * @return the total number of miles driven.
+     */
+    public int getDistanceDriven()
+    {
+        return 0;
+    }
     
     
     
@@ -91,9 +127,3 @@ public class MileageTracker
         //vin = vin;
     }
 }
-
-
-
-
-
-
